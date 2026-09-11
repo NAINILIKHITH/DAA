c8c8b0821073d1ac405860ad96ffb357f057b76c
NAINILIKHITH <likhith.naini132763@marwadiuniversity.ac.in>
2026-09-11 11:56:21 +0530

    Add files via upload

---
+++ Practical5/knapsack.py
@@ -0,0 +1,27 @@
+def knapsack(W, val, wt):
+    n = len(wt)
+
+    dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w in range(1, W + 1):
+
+            if wt[i - 1] <= w:
+                dp[i][w] = max(
+                    val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                    dp[i - 1][w]
+                )
+            else:
+                dp[i][w] = dp[i - 1][w]
+
+    return dp[n][W]
+
+
+n = int(input("Enter number of items: "))
+
+wt = list(map(int, input("Enter weights: ").split()))
+val = list(map(int, input("Enter values: ").split()))
+
+W = int(input("Enter capacity: "))
+
+print("Maximum value =", knapsack(W, val, wt))
